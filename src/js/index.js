4beaedd56a89125c0b95ec89e6452c997580919c
Asherlr2 <73479844+asherlr23@users.noreply.github.com>
2022-12-20 21:01:43 -0500

    Import current version into repo

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+var sidebarWidthCurrent = rootStyles.getPropertyValue('--sidebarwc');
+console.log("Current size of sidebar is " + deg); 
